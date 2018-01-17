e07e49b7086d3b140cd4cc1d17200b6bf9b7c2a0
Alex Nielsen <alex.nielsen@gmail.com>
2018-01-17 00:36:59 -0500

    finished!

--- designs.js
+++ designs.js
@@ -2,9 +2,29 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+let activeColor;
 
 function makeGrid() {
+  let height = $("#input_height").val();
+  let width = $("#input_width").val();
+  $("table").empty();
 
-// Your code goes here!
-
+  for (let i = 1; i <= height; i++) {
+    $("table").append(`<tr id='row${i}'></tr>`);
+    for (let j = 1; j <= width; j++) {
+      let colNum = $(`#row${i}`);
+      colNum.append(`<td id='r${i}c${j}'></td>`);
+    }
+  }
 }
+
+$("#submit").on("click", function(event) {
+  activeColor = document.getElementById("colorPicker").value;
+  event.preventDefault();
+  makeGrid();
+});
+
+$("table").on("click", "td", function(event) {
+  activeColor = document.getElementById("colorPicker").value;
+  $(this).css("background-color", activeColor);
+});
